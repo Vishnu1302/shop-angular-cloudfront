33b44ad6b5819bbf14367992622edd7ef680bffd
Vishnu Deekshit Holachi <vishnudeekshit@gmail.com>
2023-03-11 21:52:40 +0530

    feat: task5 integrate with frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,15 +9,15 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://2ccus7cei9.execute-api.us-east-1.amazonaws.com/dev',
+    bff: 'https://3kbpcb5wn7.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
-    bff: false,
+    import: true,
+    bff: true,
     cart: false,
   },
 };
