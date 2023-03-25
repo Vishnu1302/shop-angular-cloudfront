45fcb0a2b39f2971eebe5ca236c3e2d427f562a6
Vishnu Deekshit Holachi <vishnudeekshit@gmail.com>
2023-03-26 03:32:27 +0530

    feat: authorization with local storage

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -16,26 +16,27 @@ export class ManageProductsService extends ApiService {
       );
       return EMPTY;
     }
-
+    //const formData = new FormData();formData.append('file', file);
+    this.getPreSignedUrl(file.name).subscribe(res => {
+      console.log(res);
+    })
     return this.getPreSignedUrl(file.name).pipe(
       switchMap((url) =>
-        this.http.put(url, file, {
-          headers: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            'Content-Type': 'text/csv',
-          },
-        })
+        this.http.put(url, file)
       )
     );
   }
 
   private getPreSignedUrl(fileName: string): Observable<string> {
     const url = this.getUrl('import', 'import');
-
+    const token  = localStorage.getItem('authorizationToken')?.toString() || ""
     return this.http.get<string>(url, {
       params: {
         name: fileName,
       },
+      headers: {
+        Authorization: token
+      }
     });
   }
 }
